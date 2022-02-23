f689dcfc2030f0153b70e6182bf0974567e64d56
Martin Wheeler <m87.wheeler@gmail.com>
2022-02-23 13:51:34 +0000

    chore: configure webpack

---
+++ README.md
@@ -0,0 +1,14 @@
+# Life Fintepla Expandable Google Ad Banner
+
+### Getting Started
+
+This project includes a single static index.html page which can be opened directly in the browser or using VSCode's 'Live Server' plugin.
+
+### Build
+
+```shell
+npm i
+npm run build
+```
+
+This will generate minified files/optimized PNG images in the `dist` directory in the root of the project.
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new HtmlMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [["optipng", { optimizationLevel: 5 }]],
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src", to: ".", globOptions: { ignore: ["**.js"] } }],
+    }),
+  ],
 };
