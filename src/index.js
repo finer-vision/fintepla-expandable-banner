a208f78275886a1ae6556c895a16ff161810f9c3
Martin Wheeler <m87.wheeler@gmail.com>
2022-02-23 16:00:25 +0000

    chore: add exit urls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const banner = document.getElementById("expandable-banner");
 const actionArea = document.getElementById("action-area");
 const aside = document.getElementById("aside");
 const video = document.getElementById("video");
@@ -5,6 +6,7 @@ const registerExit = document.getElementById("register-exit");
 const linkExit = document.getElementById("exit-area");
 
 let isExpanded = false;
+let mouseOver = false;
 
 function expandStartHandler() {
   aside.classList.add("aside--expanded");
@@ -52,6 +54,22 @@ Enabler.addEventListener(
   collapseFinishHandler
 );
 actionArea.addEventListener("click", actionClickHandler, false);
+actionArea.addEventListener(
+  "mouseenter",
+  () => {
+    if (!mouseOver) actionClickHandler();
+    mouseOver = true;
+  },
+  false
+);
+banner.addEventListener(
+  "mouseleave",
+  () => {
+    if (mouseOver) actionClickHandler();
+    mouseOver = false;
+  },
+  false
+);
 
 // Exit event listener
 function registerExitHandler() {
